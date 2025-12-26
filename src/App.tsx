9059094c72590eacba2a7e3f7b218164a905ebdc
kuwabara <kuwabara@jig.jp>
2025-12-26 11:35:42 +0900

    テストとして、shadcn/uiからselectをインストールして適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 function App() {
 
@@ -6,6 +13,16 @@ function App() {
       <h1 className="text-4xl font-bold text-blue-600 underline">
         Tailwind CSS v4 works!
       </h1>
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
     </div>
   )
 }
