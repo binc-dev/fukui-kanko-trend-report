3e02fdd9783f8e5d9fd92fa24b69427306dab65a
kuwabara <kuwabara@jig.jp>
2026-01-05 15:53:40 +0900

    グラフの見た目を調整した

--- src/components/parts/graph.components.tsx
+++ src/components/parts/graph.components.tsx
@@ -32,16 +32,16 @@ export function Graph() {
       <h2 className="mb-4 text-2xl font-semibold text-center">回数推移</h2>
       <ResponsiveContainer width="100%" height={400}>
         <LineChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="date" />
-          <YAxis />
+          <CartesianGrid strokeOpacity={0.3}/>
+          <XAxis dataKey="date" tick={{ fontSize: 12 }} />
+          <YAxis tick={{ fontSize: 12 }} />
           <Tooltip itemSorter={(item) => -(item.value ?? 0)} />
           <Legend itemSorter={null} />
-          <Line type="monotone" dataKey="map_views" name="地図表示" stroke="#1F77B4" strokeWidth={2} />
-          <Line type="monotone" dataKey="directions" name="地図検索" stroke="#FF7F0E" strokeWidth={2} />
-          <Line type="monotone" dataKey="search_views" name="ルート設定" stroke="#2CA02C" strokeWidth={2} />
-          <Line type="monotone" dataKey="call_clicks" name="通話" stroke="#D62728" strokeWidth={2} />
-          <Line type="monotone" dataKey="website_clicks" name="ウェブサイトクリック" stroke="#9467BD" strokeWidth={2} />
+          <Line dataKey="map_views" name="地図表示" stroke="#1F77B4" strokeWidth={3} />
+          <Line dataKey="directions" name="地図検索" stroke="#FF7F0E" strokeWidth={3} />
+          <Line dataKey="search_views" name="ルート設定" stroke="#2CA02C" strokeWidth={3} />
+          <Line dataKey="call_clicks" name="通話" stroke="#D62728" strokeWidth={3} />
+          <Line dataKey="website_clicks" name="ウェブサイトクリック" stroke="#9467BD" strokeWidth={3} />
         </LineChart>
       </ResponsiveContainer>
     </div>
