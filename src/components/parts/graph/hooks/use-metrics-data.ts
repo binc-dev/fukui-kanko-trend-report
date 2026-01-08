8b3af761b0f5a0e5aa082ef51d358dce6781c748
kuwabara <kuwabara@jig.jp>
2026-01-08 13:49:12 +0900

    型エラーを修正した

--- src/components/parts/graph/hooks/use-metrics-data.ts
+++ src/components/parts/graph/hooks/use-metrics-data.ts
@@ -25,7 +25,7 @@ export function useMetricsData() {
         updatedEntry[metric.id] = null;
       }
     });
-    return updatedEntry;
+    return updatedEntry as DataPoint;
   });
 
   return { data, reviewChartData };
