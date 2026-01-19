f54498ae875ded313eaadc23c57b10e379bd8166
kuwabara <kuwabara@jig.jp>
2026-01-19 14:12:05 +0900

    ディレクトリのタイポを修正した

--- src/components/parts/date-range-picker/index.tsx
+++ src/components/parts/date-range-picker/index.tsx
@@ -1,7 +1,7 @@
 import { useChartSettings } from "@/context/ChartSettingsContext";
-import { DayRangePicker } from "./renge-picker/day-range-picker";
-import { MonthRangePicker } from "./renge-picker/month-range-picker";
-import { WeekRangePicker } from "./renge-picker/week-range-picker";
+import { DayRangePicker } from "./range-picker/day-range-picker";
+import { MonthRangePicker } from "./range-picker/month-range-picker";
+import { WeekRangePicker } from "./range-picker/week-range-picker";
 
 export function DateRangePicker() {
   const { timeUnit } = useChartSettings();
